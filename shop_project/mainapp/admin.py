ebfb9c5c7705e0327aeefeca420234aef18eef76
Serhiy Smyslov <serhiy.smyslov@gmail.com>
2021-05-17 18:44:51 +0300

    Update models and add them view in admin panel.

--- shop_project/mainapp/admin.py
+++ shop_project/mainapp/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django import forms
 
-# Register your models here.
+from .models import *
+
+
+class LaptopCategoryChoiceField(forms.ModelChoiceField):
+    pass
+
+
+class LaptopAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return LaptopCategoryChoiceField(Category.objects.filter(slug='laptop'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class SmartphoneCategoryChoiceField(forms.ModelChoiceField):
+    pass
+
+
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return SmartphoneCategoryChoiceField(Category.objects.filter(slug='smartphone'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(CartProduct)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(Laptop, LaptopAdmin)
+admin.site.register(Customer)
--- shop_project/mainapp/models.py
+++ shop_project/mainapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 User = get_user_model()
@@ -11,11 +12,18 @@ class Category(models.Model):
     name = models.CharField(max_length=255, verbose_name='Name')
     slug = models.SlugField(unique=True)
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
+
+    class Meta:
+        abstract = True
 
     category = models.ForeignKey(Category, verbose_name='Category', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Name')
@@ -30,9 +38,11 @@ class Product(models.Model):
 
 class CartProduct(models.Model):
 
-    cart = models.ForeignKey('Cart', verbose_name='Cart', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Cart', on_delete=models.CASCADE, related_name='related_products')
     user = models.ForeignKey('Customer', verbose_name='Customer', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, verbose_name='Product', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
     qty = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Total price')
 
@@ -40,10 +50,39 @@ class CartProduct(models.Model):
         return f'Product: {self.product.title}'
 
 
+class Laptop(Product):
+
+    diagonal = models.CharField(max_length=255, verbose_name='Diagonal')
+    display_type = models.CharField(max_length=255, verbose_name='Display')
+    processor_freq = models.CharField(max_length=255, verbose_name='Processor Freq')
+    ram = models.CharField(max_length=255, verbose_name='RAM')
+    video = models.CharField(max_length=255, verbose_name='Video Cart')
+    time_without_charge = models.CharField(max_length=255, verbose_name='Battery charge')
+
+    def __str__(self):
+        return f'{self.category.name} : {self.title}'
+
+
+class Smartphone(Product):
+
+    diagonal = models.CharField(max_length=255, verbose_name='Diagonal')
+    display_type = models.CharField(max_length=255, verbose_name='Display')
+    resolution = models.CharField(max_length=255, verbose_name='Screen resolution')
+    accum_volume = models.CharField(max_length=255, verbose_name='Accum volume')
+    ram = models.CharField(max_length=255, verbose_name='RAM')
+    sd = models.BooleanField(default=True)
+    sd_volume_max = models.CharField(max_length=255, verbose_name='Max ROM')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='Main camera')
+    frontal_cam_mp = models.CharField(max_length=255, verbose_name='Frontal camera')
+
+    def __str__(self):
+        return f'{self.category.name} : {self.title}'
+
+
 class Cart(models.Model):
 
     owner = models.ForeignKey('Customer', verbose_name='Owner', on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product, blank=True)
+    products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
     total_products = models.PositiveIntegerField(default=0)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Total price')
 
@@ -59,13 +98,3 @@ class Customer(models.Model):
 
     def __str__(self):
         return f'Customer: {self.user.first_name} {self.user.last_name}'
-
-
-class Specification(models.Model):
-
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name='Product name')
-
-    def __str__(self):
-        return f'characteristic for product: {self.name}'
